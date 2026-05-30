4731801eff30e4364c831d4d5b1342a70cd957d7
Ssn-j <2102353749@qq.com>
2026-05-30 14:53:52 +0800

    auto sync: 周六 2026/05/30 14:53:52.32

--- 1.py
+++ 1.py
@@ -1,10 +1,2 @@
-class Student:
-    def __init__(self, name, age, secret):
-        self.name = name
-        self._age = age
-        self.__secret = secret
-    
-student = Student("John", 18, "secret")
-print(student.name)  # 输出: John
-print(student._age)  # 输出: 18
-# print(student.__secret)  # 输出: AttributeError: 'Student' object has no attribute '__secret'
+import abb
+print(abb.path) # 查看所有搜索路径
